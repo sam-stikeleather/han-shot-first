73ab6ecc370e308354792943fb87ea76225dfbf4
Sam <sam.stikeleather@gmail.com>
2023-06-04 17:41:26 -0400

    Added Prompts boxes and console logs

--- script.js
+++ script.js
@@ -10,5 +10,31 @@ function writePassword() {
 
 }
 
+var upper = prompt("Would you like upper case letters? Y or N");
+console.log("Upper: " + upper);
+
+var numbers = prompt("Would you like to include numbers? Y or N");
+console.log("Numbers: " + numbers);
+
+var characters = prompt("Would you like to include special characters? i.e. !@#$% Y or N");
+console.log("Special: " + characters);
+
+var prompt = prompt("Please indicate a password length between 8 - 128 characters.");
+console.log("Length: " + prompt);
+
+
+
+var generatePassword = (
+  length = 20,
+  wishlist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
+) =>
+  Array.from(crypto.getRandomValues(new Uint32Array(length)))
+    .map((x) => wishlist[x % wishlist.length])
+    .join('')
+
+console.log(generatePassword())
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
